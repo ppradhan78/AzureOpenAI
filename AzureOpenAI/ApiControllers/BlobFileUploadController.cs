e27577c953ea21f0bcfc63dd6589dff304439218
pradhan prasanta <pradhan.prasanta78@gmail.com>
2024-03-01 20:12:27 +0530

    Added AzureOpenAI

---
+++ AzureOpenAI/ApiControllers/BlobFileUploadController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureOpenAI.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlobFileUploadController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> UploadBlobs(List<IFormFile> files)
+        {
+            return Ok();
+        }
+    }
+}
